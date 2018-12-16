0541f4e4889fa55e848e8ae7a8ee9f31f2c29c6d
Imtiyaz-CHOUJAI <choujai.imtiyaz@gmail.com>
2018-12-17 00:26:24 +0100

    Create Login.js

---
+++ src/components/Login.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <div className="Login">
+                <h1 className="primary-heading">Log in</h1>
+
+                <p className="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                <a href="#0" className="btn btn-dark-blue">Login with BankID</a>
+                <a href="#0" className="btn btn-fb-blue">Login with Facebook</a>
+
+                <div>
+                    <form className="form">
+                        <div className="u-margin-top-bottom-small">
+                            <h2 className="secondary-heading">
+                                Log in using email
+                            </h2>
+                        </div>
+
+                        <div className="form__group">
+                            <input id="email" type="email" className="form__input" placeholder="Email" required />
+                            <label for="email" className="form__label">Email</label>
+                        </div>
+
+                        <div className="form__group">
+                            <input id="password" type="password" className="form__input" placeholder="Password" required />
+                            <label for="password" className="form__label">Password</label>
+                        </div>
+
+                        <div className="u-margin-top-bottom-small">
+                            <a className="btn btn-light-blue" href="#0">Log in</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
